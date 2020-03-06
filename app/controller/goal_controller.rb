856023700759057885f0cfca6979dd2caa07497a
kellypowers <kpowers.nj@gmail.com>
2020-03-05 23:34:35 -0500

    adds form for new goals

--- app/controller/goal_controller.rb
+++ app/controller/goal_controller.rb
@@ -1,3 +1,30 @@
 class GoalController < ApplicationController
 
+    get '/goals/new' do 
+        @user = User.find_by_id(params[:id])
+        erb :'/goals/new'
+    end
+
+    get '/goals/:id' do 
+        @user = User.find(session[:user_id])
+        @goal = Goal.find_by_id(params[:id])
+        erb :'/goals/show'
+    end
+
+    get '/goals/index' do 
+        @user = User.find(session[:user_id])
+        erb :'/goals/index'
+    end
+
+    post '/goals' do 
+        @user = User.find(session[:user_id])
+        @goal = Goal.create(params["goal"])
+        @goal.user_id = @user.id 
+        @goal.save
+        redirect to '/users/home'
+    end
+
+    
+
+
 end
