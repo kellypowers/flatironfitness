73f13dc12596e3911338eb21e1f3f6f1ee19a3e7
kellypowers <kpowers.nj@gmail.com>
2020-10-14 20:59:22 -0400

    prepping for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,28 +1,28 @@
 
-# # ENV['SINATRA_ENV'] ||= "development"
-# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+# ENV['SINATRA_ENV'] ||= "development"
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-# require 'bundler/setup'
+require 'bundler/setup'
 # Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#   # :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-#   :database => db.path[1..-1]
-# )
+ActiveRecord::Base.establish_connection(
+  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  # :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database => db.path[1..-1]
+)
 
-# require_all 'app'
+require_all 'app'
 
 
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+# configure :production do
+#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-    )
-end
+#   ActiveRecord::Base.establish_connection(
+#     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host     => db.host,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => db.path[1..-1],
+#     :encoding => 'utf8'
+#     )
+# end
