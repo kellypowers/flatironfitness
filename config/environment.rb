1eaf6feeb3f6255f19f38468e47d532087d30470
kellypowers <kpowers.nj@gmail.com>
2020-10-14 21:12:13 -0400

    prepping for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,32 +1,3 @@
-
-# ENV['SINATRA_ENV'] ||= "development"
-# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-# require 'bundler/setup'
-# # Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# ActiveRecord::Base.establish_connection(
-#   :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#   # :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-#   :database => db.path[1..-1]
-# )
-
-# require_all 'app'
-
-
-# configure :production do
-#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-#   ActiveRecord::Base.establish_connection(
-#     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#     :host     => db.host,
-#     :username => db.user,
-#     :password => db.password,
-#     :database => db.path[1..-1],
-#     :encoding => 'utf8'
-#     )
-# end
-
 configure :development do
   ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
